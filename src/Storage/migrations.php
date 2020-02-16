6109d2d9a11c742a305bce155d4ebea7a6cb22a1
nanorocks <andrejnankov@gmail.com>
2020-02-17 00:07:51 +0100

    UPD on controllers db provider and config

--- src/Storage/migrations.php
+++ src/Storage/migrations.php
@@ -10,7 +10,7 @@ $container = \App\Bootstrap\Container::get();
 $container->register(new \App\Provider\ConfigProvider());
 $container->register(new \App\Provider\DatabaseProvider());
 
-$capsule = $container['db'];
+$capsule = $container['db']['capsule'];
 
 $capsule::schema()->create('urls', function ($table) {
     $table->increments('id');
@@ -18,3 +18,8 @@ $capsule::schema()->create('urls', function ($table) {
     $table->string('path')->unique();
     $table->timestamps();
 });
+
+//Illuminate\Database\Capsule\Manager::table('urls')->insert([
+//    'link' => 'fb.com',
+//    'path' => 'asf234'
+//]);
