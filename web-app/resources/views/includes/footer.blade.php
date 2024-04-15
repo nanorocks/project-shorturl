a06cd580bf7cd011214c82663240e6f9b427d642
Andrej Nankov <andrejnankov@gmail.com>
2024-04-16 00:35:20 +0200

    Fix ui

--- web-app/resources/views/includes/footer.blade.php
+++ web-app/resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <div class="text-center pb-2 fixed-bottom h5 text-white" style="font-weight: 100; font-size: 1rem">
-    <div class="small-divider text-center mt-5"></div>
+    {{-- <div class="small-divider text-center mt-5"></div> --}}
     <br />
     Developed by <a href="https://nankov.mk" class="text-white">@nanorocks</a>
     <div class="badge d-block small ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-center sm:ml-0">
