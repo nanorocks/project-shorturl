eff5d09c1d1317968152a04865097aa79c66de2e
Andrej Nankov <andrejnankov@gmail.com>
2024-04-15 23:05:45 +0200

    Move to php 8.2

--- web-app/resources/views/includes/footer.blade.php
+++ web-app/resources/views/includes/footer.blade.php
@@ -2,4 +2,7 @@
     <div class="small-divider text-center mt-5"></div>
     <br />
     Developed by <a href="https://nankov.mk" class="text-decoration-none">@nanorocks</a>
+    <div class="small ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-center sm:ml-0">
+        Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+    </div>
 </div>
