4a320c29f02e3c87be568dc06e0f86e90b065517
Andrej Nankov <andrejnankov@gmail.com>
2024-04-21 12:50:52 +0200

    Hotfix loading and login

--- web-app/resources/views/home.blade.php
+++ web-app/resources/views/home.blade.php
@@ -134,9 +134,7 @@
 
     @include('includes.footer')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
-    </script>
+
     <script>
         function onSubmit() {
             grecaptcha.execute();
@@ -145,6 +143,10 @@
         function onCompleted(token) {
             document.getElementById("submit-form").submit();
         }
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
     </script>
 </body>
 
