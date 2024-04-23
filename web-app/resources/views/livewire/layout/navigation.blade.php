bd70018716e055c1b6ff56a79cc9dd352479f1c0
Andrej Nankov <andrejnankov@gmail.com>
2024-04-24 00:14:25 +0200

    Add new logos

--- web-app/resources/views/livewire/layout/navigation.blade.php
+++ web-app/resources/views/livewire/layout/navigation.blade.php
@@ -23,7 +23,9 @@ new class extends Component {
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}" wire:navigate>
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <img class="d-block mx-auto img-fluid" src="{{ asset('img/logo1.png') }}" alt="logo"
+                            width="40" style="border-radius: 20%">
+                        {{-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" /> --}}
                     </a>
                 </div>
                 <?php
